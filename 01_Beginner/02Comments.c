ad456e3fdec3d56b3cb91329bf34809d96da9e5a
Enijeshiku Eniola <eenijeshiku@gmail.com>
2025-07-27 17:35:46 +0100

    Add examples of single-line and multi-line comments in Comments lesson

---
+++ 01_Beginner/02Comments.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int maain () 
+{
+    //Basic Arithemetic Operation
+    int result = 10 + 5;
+
+    // Output
+    printf("Result is: %d\n", result);
+
+    /* MULTI LINE COMMENT */
+    return 0;//Program end
+}
