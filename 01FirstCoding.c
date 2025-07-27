ea79ba60fccea2ef0b6cd8baf53d2f461b6c4e58
Enijeshiku Eniola <eenijeshiku@gmail.com>
2025-07-27 16:27:06 +0100

    Add first C programs: print statements, variables, and formatted

---
+++ 01FirstCoding.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main () 
+{
+   printf("This is my First Coding in C\n"); 
+
+   int num = 42;
+   printf ("Answer is: %d\n", num);
+
+   float pi = 3.14159;
+   printf("Value of pi= %.3f\n", pi);
+   return 0;
+}
