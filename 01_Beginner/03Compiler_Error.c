1680b1b5bacb96befc416790b20d62f2856f00cb
Enijeshiku Eniola <eenijeshiku@gmail.com>
2025-07-28 11:38:52 +0100

    Add examples of common compiler errors in Compiler_Error lesson

---
+++ 01_Beginner/03Compiler_Error.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main () 
+{
+    printf("Hello World\n");
+    printf("expected a ';'\n");
+
+    printf("undefined reference to function_name \n");
+    printf("unrecognized token\n");
+    
+    printf("expected ';' before '}' token");
+    printf("Error Type: Syntax Error");
+    
+    return 0;
+}
