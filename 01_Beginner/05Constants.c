c7381218e356b082740c0aedad869696e2ed1a65
Enijeshiku Eniola <eenijeshiku@gmail.com>
2025-07-29 20:44:05 +0100

    Learnt Constants in C

---
+++ 01_Beginner/05Constants.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main () 
+{
+    const float Pi = 3.1416;
+    const char* GREETING = "H";
+
+    printf("Value of PI:%.2f\n", Pi);
+    printf("Greeting: %s\n", GREETING);
+
+    return 0;
+}
