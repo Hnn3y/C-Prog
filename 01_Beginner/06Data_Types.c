9f03bba79da245f507f8761ad0128fb7a3347d74
Enijeshiku Eniola <eenijeshiku@gmail.com>
2025-07-30 22:15:07 +0100

    06 Data Types

--- 01_Beginner/06Data_Types.c
+++ 01_Beginner/06Data_Types.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 int main () 
 {
-    
+    int integer = 42;
+
+    float floatValue = 3.14;
+    char charValue = 'B';
+
+    printf("integer:%d", integer);
+
+    return 0;
 }
