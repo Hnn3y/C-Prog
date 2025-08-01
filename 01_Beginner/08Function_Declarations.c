079affb4fd899f28c6e2a438e680e182f683ef7d
Enijeshiku Eniola <eenijeshiku@gmail.com>
2025-08-01 22:34:31 +0100

    Lesson 8 function declaration

---
+++ 01_Beginner/08Function_Declarations.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void greetUser (char name[]);
+int main () 
+{
+    greetUser("Bob");
+
+    return 0; 
+}
+
+void greetUser (char name[]) {
+    printf("Welcome to function decelartion\n");
+}
